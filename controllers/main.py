3ec1efd12537d991d24db2fc55c88357ae9288a2
kitaniboy <abdullahalkitani@gmail.com>
2024-02-15 07:13:10 +0000

    Removed webhook endpoint for now

--- controllers/main.py
+++ controllers/main.py
@@ -59,51 +59,6 @@ class ThawaniPayController(http.Controller):
         
         return request.redirect('/payment/status')
 
-    @http.route(_webhook_url, type='http', auth='public', methods=['POST'], csrf=False)
-    def thawani_webhook(self, **data):
-        """ Process the notification data sent by Thawani to the webhook.
-
-        :param dict data: The notification data.
-        :return: The 'OK' string to acknowledge the notification.
-        :rtype: str
-        """
-        _logger.info("Notification received from Thawani with data:\n%s", pprint.pformat(data))
-        try:
-            # Check the integrity of the notification data.
-            tx_sudo = request.env['payment.transaction'].sudo()._get_tx_from_notification_data(
-                'thawani', data
-            )
-            self._verify_notification_signature(data, tx_sudo)
-
-            # Handle the notification data.
-            tx_sudo._handle_notification_data('thawani', data)
-        except ValidationError:  # Acknowledge the notification to avoid getting spammed.
-            _logger.exception("Unable to handle the notification data; skipping to acknowledge.")
-
-        return 'OK'  # Acknowledge the notification.
-
-    @staticmethod
-    def _verify_notification_signature(notification_data, tx_sudo):
-        """ Check that the received signature matches the expected one.
-
-        :param dict notification_data: The notification data
-        :param recordset tx_sudo: The sudoed transaction referenced by the notification data, as a
-                                  `payment.transaction` record
-        :return: None
-        :raise: :class:`werkzeug.exceptions.Forbidden` if the signatures don't match
-        """
-        received_signature = notification_data.get('secureHash')
-        if not received_signature:
-            _logger.warning("Received notification with missing signature.")
-            raise Forbidden()
-
-        # Compare the received signature with the expected signature computed from the data.
-        expected_signature = tx_sudo.provider_id._thawani_calculate_signature(
-            notification_data, incoming=True
-        )
-        if not hmac.compare_digest(received_signature, expected_signature):
-            _logger.warning("Received notification with invalid signature.")
-            raise Forbidden()
     
     @staticmethod
     def _verify_payment_status(notification_data, tx_sudo, possible_payment_statuses: set):
